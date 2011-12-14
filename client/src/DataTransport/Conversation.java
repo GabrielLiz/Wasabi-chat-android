c6fb2934f8ccdb08a3c66f795575f42afb036f56
unknown <Med_Regaya@Med_Regaya-PC.(none)>
2011-12-14 11:02:46 +0100

    source

---
+++ client/src/DataTransport/Conversation.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package DataTransport;
+
+/**
+ * @author Yannick Lanz
+ *
+ */
+public class Conversation extends AdminStream {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4479215220670353994L;
+
+	/**
+	 * 
+	 */
+	public Conversation() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ client/src/DataTransport/Stream.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package DataTransport;
+
+import java.io.Serializable;
+
+/**
+ * @author Yannick Lanz
+ *
+ */
+abstract public class Stream implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1982056651442244303L;
+}
