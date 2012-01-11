8b1acef905d164d5229553ac14b7a65f46139b7e
Steeve Werren <steeve.werren@heig-vd.ch>
2012-01-11 18:25:01 +0100

    Ajout du cas d'utilisation <ajouter contact> et <confirmer une demande> au niveau de l'interface

--- WasabiAndroid/gen/com/exemple/clientchat/R.java
+++ WasabiAndroid/gen/com/exemple/clientchat/R.java
@@ -12,34 +12,47 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
-        public static final int wasabi=0x7f020001;
+        public static final int photo=0x7f020001;
+        public static final int wasabi=0x7f020002;
     }
     public static final class id {
-        public static final int clientList=0x7f050009;
-        public static final int clientNameLbl=0x7f05000a;
-        public static final int createBtn=0x7f050006;
-        public static final int infoCreate=0x7f050007;
-        public static final int loginBtn=0x7f050004;
-        public static final int loginLbl=0x7f050000;
-        public static final int loginText=0x7f050001;
-        public static final int messageTxt=0x7f05000f;
-        public static final int passConfText=0x7f050005;
-        public static final int passLbl=0x7f050002;
-        public static final int passText=0x7f050003;
-        public static final int restoreDateText=0x7f050008;
-        public static final int scrollView=0x7f05000b;
-        public static final int sendBtn=0x7f05000e;
-        public static final int tchatLbl=0x7f05000c;
-        public static final int tchatText=0x7f05000d;
+        public static final int AddContact=0x7f05000b;
+        public static final int ContactBtn=0x7f050002;
+        public static final int ContactLabel=0x7f050000;
+        public static final int ContactText=0x7f050001;
+        public static final int RequestContact=0x7f05000c;
+        public static final int RequestList=0x7f05000f;
+        public static final int clientList=0x7f05000e;
+        public static final int clientNameLbl=0x7f050013;
+        public static final int createBtn=0x7f050009;
+        public static final int description=0x7f050012;
+        public static final int img=0x7f050010;
+        public static final int infoCreate=0x7f05000a;
+        public static final int loginBtn=0x7f050007;
+        public static final int loginLbl=0x7f050003;
+        public static final int loginText=0x7f050004;
+        public static final int messageTxt=0x7f050018;
+        public static final int passConfText=0x7f050008;
+        public static final int passLbl=0x7f050005;
+        public static final int passText=0x7f050006;
+        public static final int restoreDateText=0x7f05000d;
+        public static final int scrollView=0x7f050014;
+        public static final int sendBtn=0x7f050017;
+        public static final int tchatLbl=0x7f050015;
+        public static final int tchatText=0x7f050016;
+        public static final int titre=0x7f050011;
     }
     public static final class layout {
-        public static final int authent=0x7f030000;
-        public static final int create=0x7f030001;
-        public static final int error_create=0x7f030002;
-        public static final int list=0x7f030003;
-        public static final int main=0x7f030004;
-        public static final int tchat=0x7f030005;
-        public static final int window=0x7f030006;
+        public static final int addcontact=0x7f030000;
+        public static final int authent=0x7f030001;
+        public static final int create=0x7f030002;
+        public static final int error_create=0x7f030003;
+        public static final int list=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int request=0x7f030006;
+        public static final int requestview=0x7f030007;
+        public static final int tchat=0x7f030008;
+        public static final int window=0x7f030009;
     }
     public static final class string {
         public static final int Wasabi=0x7f040000;
--- WasabiAndroid/src/com/exemple/clientchat/ClientTchat.java
+++ WasabiAndroid/src/com/exemple/clientchat/ClientTchat.java
@@ -1,20 +1,24 @@
 package com.exemple.clientchat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import com.exemple.clientchat.R;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ScrollView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.view.LayoutInflater;
-import android.widget.Toast;
 
 public class ClientTchat extends Activity {
 	
@@ -128,5 +132,100 @@ public class ClientTchat extends Activity {
     	messageTxt.append("\n");
     	messageTxt.append(msg);
     	edit.setText("");
+    }
+    
+    
+    /**
+     * Add a contact
+     * @param btn
+     */
+    public void ViewAddContact(View btn){
+    	setContentView(R.layout.addcontact);
+
+    }
+    
+    /**
+     * Add a contact
+     * @param btn
+     */
+    public void addContact(View btn){
+    	//TODO Conecter au serveur pour ajouter contact
+    	getClientList(btn);
+    }
+    
+    private ListView requestList;
+    /**
+     * View request
+     * @param btn
+     */
+    public void ViewRequest(View btn){
+    	//TODO Conecter au serveur pour voir les demande d'ajout
+    	setContentView(R.layout.request);
+    	 
+        //Récupération de la listview créée dans le fichier request.xml
+    	requestList = (ListView) findViewById(R.id.RequestList);
+ 
+        //Création de la ArrayList qui nous permettra de remplire la listView
+        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
+ 
+        //On déclare la HashMap qui contiendra les informations pour un item
+        HashMap<String, String> map;
+ 
+        //Création d'une HashMap pour insérer les informations du premier item de notre listView
+        map = new HashMap<String, String>();
+        //on insère un élément titre que l'on récupérera dans le textView titre créé dans le fichier affichageitem.xml
+        map.put("titre", "Jim");
+        //on insère un élément description que l'on récupérera dans le textView description créé dans le fichier affichageitem.xml
+        map.put("description", "Saluta ajoute moi stp...");
+        //on insère la référence à l'image (convertit en String car normalement c'est un int) que l'on récupérera dans l'imageView créé dans le fichier affichageitem.xml
+        map.put("img", String.valueOf(R.drawable.photo));
+        //enfin on ajoute cette hashMap dans la arrayList
+        listItem.add(map);
+ 
+        //On refait la manip plusieurs fois avec des données différentes pour former les items de notre ListView
+ 
+        map = new HashMap<String, String>();
+        map.put("titre", "Murphy");
+        map.put("description", "Add me plz");
+        map.put("img", String.valueOf(R.drawable.photo));
+        listItem.add(map);
+ 
+        map = new HashMap<String, String>();
+        map.put("titre", "Lara");
+        map.put("description", "Hello tu m'ajoute?");
+        map.put("img", String.valueOf(R.drawable.photo));
+        listItem.add(map);
+ 
+        map = new HashMap<String, String>();
+        map.put("titre", "Jerry");
+        map.put("description", "ajoute moi!");
+        map.put("img", String.valueOf(R.drawable.photo));
+        listItem.add(map);
+ 
+        //Création d'un SimpleAdapter qui se chargera de mettre les items présent dans notre list (listItem) dans la vue request
+        SimpleAdapter mSchedule = new SimpleAdapter (this.getBaseContext(), listItem, R.layout.requestview,
+               new String[] {"img", "titre", "description"}, new int[] {R.id.img, R.id.titre, R.id.description});
+ 
+        //On attribut à notre listView l'adapter que l'on vient de créer
+        requestList.setAdapter(mSchedule);
+ 
+        //Enfin on met un écouteur d'évènement sur notre listView
+        requestList.setOnItemClickListener(new OnItemClickListener() {
+			@SuppressWarnings("unchecked")
+         	public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+				//on récupère la HashMap contenant les infos de notre item (titre, description, img)
+        		HashMap<String, String> map = (HashMap<String, String>) requestList.getItemAtPosition(position);
+        		//on créer une boite de dialogue
+        		AlertDialog.Builder adb = new AlertDialog.Builder(ClientTchat.this);
+        		//on attribut un titre à notre boite de dialogue
+        		adb.setTitle("Sélection Item");
+        		//on insère un message à notre boite de dialogue, et ici on affiche le titre de l'item cliqué
+        		adb.setMessage("Votre choix : "+map.get("titre"));
+        		//on indique que l'on veut le bouton ok à notre boite de dialogue
+        		adb.setPositiveButton("Ok", null);
+        		//on affiche la boite de dialogue
+        		adb.show();
+        	}
+         });
     }
 }
